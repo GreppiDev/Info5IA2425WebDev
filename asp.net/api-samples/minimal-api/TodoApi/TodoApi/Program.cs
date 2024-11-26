2aa489475f5fb78646389ff5bf355391a237d436
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2024-11-26 19:42:13 +0100

    Aggiunta una classe DTO - Aggiunta una classe DTO per la rappresentazione di un oggetto Todo - DTO è l'acronimo di Data Transfer Object

--- asp.net/api-samples/minimal-api/TodoApi/TodoApi/Program.cs
+++ asp.net/api-samples/minimal-api/TodoApi/TodoApi/Program.cs
@@ -59,40 +59,47 @@ app.Run();
 
 //metodi richiamati dagli Endpoint Routes 
 
-static async Task<Ok<Todo[]>> GetAllTodos(TodoDb db)
+static async Task<Ok<TodoItemDTO[]>> GetAllTodos(TodoDb db)
 {
-	return TypedResults.Ok(await db.Todos.ToArrayAsync());
+	return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
 }
 
-static async Task<Ok<List<Todo>>> GetCompleteTodos(TodoDb db)
+static async Task<Ok<List<TodoItemDTO>>> GetCompleteTodos(TodoDb db)
 {
-	return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+	return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
 }
 
-static async Task<Results<Ok<Todo>, NotFound>> GetTodo(int id, TodoDb db)
+static async Task<Results<Ok<TodoItemDTO>, NotFound>> GetTodo(int id, TodoDb db)
 {
 	return await db.Todos.FindAsync(id)
 		is Todo todo
-			? TypedResults.Ok(todo)
+			? TypedResults.Ok(new TodoItemDTO(todo))
 			: TypedResults.NotFound();
 }
 
-static async Task<Created<Todo>> CreateTodo(Todo todo, TodoDb db)
+static async Task<Created<TodoItemDTO>> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
 {
-	db.Todos.Add(todo);
+	var todoItem = new Todo
+	{
+		Name = todoItemDTO.Name,
+		IsComplete = todoItemDTO.IsComplete
+	};
+	db.Todos.Add(todoItem);
 	await db.SaveChangesAsync();
+	
+	todoItemDTO = new TodoItemDTO(todoItem);
 
-	return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+	return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
 }
 
-static async Task<Results<NotFound, NoContent>> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+static async Task<Results<NotFound, NoContent>> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
 {
 	var todo = await db.Todos.FindAsync(id);
 
 	if (todo is null) return TypedResults.NotFound();
 
-	todo.Name = inputTodo.Name;
-	todo.IsComplete = inputTodo.IsComplete;
+	todo.Name = todoItemDTO.Name;
+	todo.IsComplete = todoItemDTO.IsComplete;
 
 	await db.SaveChangesAsync();
 
