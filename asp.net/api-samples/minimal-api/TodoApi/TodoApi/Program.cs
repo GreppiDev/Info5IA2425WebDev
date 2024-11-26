84f01054c2e6a67d2df5f0a2ca051f09c482b8a2
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2024-11-26 19:00:35 +0100

    Uso di metodi per gli Endpoint RouteHandler -- i metodi utilizzano IResult per restituire un risultato, come mostrato nel tutoria Microsoft https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-ap tuttavia, OpenAPi non è in grado di identificare il tipo di risultato restituito

--- asp.net/api-samples/minimal-api/TodoApi/TodoApi/Program.cs
+++ asp.net/api-samples/minimal-api/TodoApi/TodoApi/Program.cs
@@ -45,108 +45,76 @@ if (app.Environment.IsDevelopment())
 
 //creations of API routes
 app.MapGet("/", () => "Hello World!");
-//uso di MapGroup per raggruppare le API
 var todoItems = app.MapGroup("/todoitems");
-//get all todoitems
-todoItems.MapGet("/", async (TodoDb db) => TypedResults.Ok( await db.Todos.ToListAsync()));
 
-//get all completed todoitems
-//si noti che le lambda expressions permettono di inferire il tipo di ritorno
-//ad esempio in questo caso il tipo di ritorno è List<Todo>
-// la lambda expression è async Task<Ok<List<Todo>>> (TodoDb db) => TypedResults.Ok( await db.Todos.Where(x => x.IsComplete).ToListAsync())
-todoItems.MapGet("/complete", async (TodoDb db) => TypedResults.Ok( await db.Todos.Where(x => x.IsComplete).ToListAsync()));
-// todoItems.MapGet("/complete", async (TodoDb db) => {
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
 
-// 	 var results = await db.Todos.Where(x => x.IsComplete).ToListAsync();
-// 	 return Results.Ok(results);
 
-// 	});
+app.Run();
 
-//get specific todoitem
-//nel caso in cui si utilizzi TypedResults al posto di Results è possibile specificare il tipo di ritorno
-//ma bisogna stare attenti a come si gestiscono i tipi di ritorno
-//nel caso in cui si utilizzi Results è possibile specificare il tipo di ritorno in modo più semplice
-//nel caso in cui si utilizzi TypedResults è necessario specificare il tipo di ritorno in modo più esplicito
-// ad esempio in questo caso si specifica che il tipo di ritorno è Results<Ok<Todo>, NotFound>
-//ma sarebbe anche possibile specificare il tipo di ritorno come IResult
-//In cosa sta la differenza?
-//TypedResults.Ok(todo) ritorna un oggetto di tipo Ok<Todo>
-//Results.Ok(todo) ritorna un oggetto di tipo Ok
-//quindi se si utilizza TypedResults è necessario specificare il tipo di ritorno in modo più esplicito.
+//metodi richiamati dagli Endpoint Routes 
+//in questo caso i metodi espongono un tipo di ritorno IResult
+//che è un'interfaccia che rappresenta un risultato di una richiesta HTTP
+//e può essere un risultato di successo o di errore
 
-//La differenza sta anche nell'utilizzo di OpenAPI, infatti se si utilizza TypedResults è possibile specificare il tipo di ritorno
-//in modo più esplicito e quindi OpenAPI può generare una documentazione più precisa
+//Con questi metodi OpenAPI può generare la documentazione automaticamente
+//ma non è in grado di definire i tipi di ritorno dei metodi, perché, a differenza
+//delle lambda expression, i metodi devono dichiarare il tipo di ritorno e non 
+//c'è una inferenza di tipo come avviene con le lambda expression
 
-todoItems.MapGet("/{id}", async Task<Results<Ok<Todo>, NotFound>> (TodoDb db, int id) =>
+static async Task<IResult> GetAllTodos(TodoDb db)
 {
-	var todo = await db.Todos.FindAsync(id);
-	return todo is not null ? TypedResults.Ok(todo) : TypedResults.NotFound();
-});
-// {
-// 	var todo = await db.Todos.FindAsync(id);
-// 	if (todo == null)
-// 	{
-// 		return Results.NotFound();
-// 	}
-// 	return Results.Ok(todo);
-// });
+	return TypedResults.Ok(await db.Todos.ToArrayAsync());
+}
 
-//insert specific todoitem
-//here we use Dependency Injection 
-todoItems.MapPost("/", async (TodoDb db, Todo todo) =>
+static async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+	return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+}
 
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+	return await db.Todos.FindAsync(id)
+		is Todo todo
+			? TypedResults.Ok(todo)
+			: TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
 {
 	db.Todos.Add(todo);
 	await db.SaveChangesAsync();
-	return Results.Created($"/todoitems/{todo.Id}", todo);
-});
 
-//update specific todoitem
-todoItems.MapPut("/{id}", async (TodoDb db, int id, Todo todo) =>
+	return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
 
+static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
 {
-	//check if the id in the URL matches the id in the body
-	if (id != todo.Id)
-	{
-		return Results.BadRequest("Id mismatch");
-	}
-	var existing = await db.Todos.FindAsync(id);
-	//check if the todoitem exists
-	if (existing == null)
-	{
-		return Results.NotFound();
-	}
-	//update the todoitem
-	existing.Name = todo.Name;
-	existing.IsComplete = todo.IsComplete;
+	var todo = await db.Todos.FindAsync(id);
+
+	if (todo is null) return TypedResults.NotFound();
+
+	todo.Name = inputTodo.Name;
+	todo.IsComplete = inputTodo.IsComplete;
+
 	await db.SaveChangesAsync();
-	//return a 204 No Content response
-	return Results.NoContent();
-});
 
-//delete specific todoitem
-todoItems.MapDelete("/{id}", async (TodoDb db, int id) =>
+	return TypedResults.NoContent();
+}
 
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
 {
-	// //verify if the todoitem exists
-	// var todo = await db.Todos.FindAsync(id);
-	// if (todo == null)
-	// {
-	// 	return Results.NotFound();
-	// }
-	// //remove the todoitem
-	// db.Todos.Remove(todo);
-	// await db.SaveChangesAsync();
-	// //return a 204 No Content response
-	// return Results.NoContent();
-
 	if (await db.Todos.FindAsync(id) is Todo todo)
 	{
 		db.Todos.Remove(todo);
 		await db.SaveChangesAsync();
-		return Results.NoContent();
+		return TypedResults.NoContent();
 	}
-	return Results.NotFound();
-});
 
-app.Run();
+	return TypedResults.NotFound();
+}
